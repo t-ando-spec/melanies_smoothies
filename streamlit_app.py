2560b5fffc79fc546c06056207a8f330c9c7b5a4
t-ando-spec <t-ando@weing.co.jp>
2026-06-12 17:57:34 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,6 @@ import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
-streamlit.title('My Parents New Healthy Diner')
-
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie!:cup_with_straw:")
 st.write(
